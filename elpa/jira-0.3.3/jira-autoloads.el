f5289ce597eb8ac1b056da31a802b1aaacbba88d
jtimberman <joshua@opscode.com>
2011-10-28 20:54:49 -0600

    jira-mode from elpa

---
+++ elpa/jira-0.3.3/jira-autoloads.el
@@ -0,0 +1,18 @@
+;;; jira-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("jira-pkg.el" "jira.el") (20139 15280
+;;;;;;  143307))
+
+;;;***
+
+(provide 'jira-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; jira-autoloads.el ends here
