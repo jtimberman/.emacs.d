e1c8483ac90c0cdaa2835691dce44a657bfc7832
jtimberman <joshua@opscode.com>
2011-11-27 10:07:40 -0700

    replace irblack w/ ir-black, tuned some colors

--- jtimberman/themes/color-theme-ir-black.el
+++ jtimberman/themes/color-theme-ir-black.el
@@ -1,4 +1,4 @@
-;;; color-theme-ir-black.el --- pastel color theme 
+;;; color-theme-ir-black.el --- pastel color theme
 ;; MIT License Copyright (c) 2009 Burke Libbey <burke@burkelibbey.org>
 ;; URL: https://github.com/kurtharriger/color-theme-ir-black
 ;; Author: Burke Libbey, Kurt Harriger
@@ -11,9 +11,9 @@
 
   (let ((*normal*       "#F6F3E8")
         (*background*   "#000000")
-        (*cursor*       "#FFA560")
+        (*cursor*       "#8888AA")
         (*mouse*        "sienna1")
-        (*region*       "#1D1E2C")
+        (*region*       "#353883")
         (*current-line* "#151515")
         (*string*       "#A8FF60")
         (*keyword*      "#66B5FF")
@@ -72,8 +72,36 @@
          (toolbar ((t (nil))))
          (bold ((t (:bold t))))
          (bold-italic ((t (:bold t))))
-         (underline ((nil (:underline nil)))))))))
+         (underline ((nil (:underline nil))))
+         (ido-subdir ((t (:foreground "#CF6A4C"))))
+         (ido-first-match ((t (:foreground "#8F9D6A"))))
+         (ido-only-match ((t (:foreground "#8F9D6A"))))
+         (show-paren-mismatch ((t (:background "#881100"))))
+         ;; mumamo
+         (mumamo-background-chunk-major ((t (:background "#000000"))))
+         (mumamo-background-chunk-submode1 ((t (:background "#0A0A0A"))))
+         (mumamo-background-chunk-submode2 ((t (:background "#0A0A0A"))))
+         (mumamo-background-chunk-submode3 ((t (:background "#0A0A0A"))))
+         (mumamo-background-chunk-submode4 ((t (:background "#0A0A0A"))))
 
+         ;; diff-mode
+         (diff-added ((t (:background "#253B22" :foreground "#F8F8F8"))))
+         (diff-removed ((t (:background "#420E09" :foreground "#F8F8F8"))))
+         (diff-content ((t nil)))
+         (diff-header ((t (:background "#0E2231" :foreground "#F8F8F8"))))
+
+         ;; nxml
+         (nxml-delimiter ((t (:foreground "#96CBFE"))))
+         (nxml-name ((t (:foreground "#96CBFE"))))
+         (nxml-element-local-name ((t (:foreground "#96CBFE"))))
+         (nxml-attribute-local-name ((t (:foreground "#FFD7B1"))))
+
+         ;; flymake
+         (flymake-errline ((((class color)) (:background "#441111"))))
+         (flymake-warnline ((((class color)) (:background "yellow"))))
+         )))))
+
+(add-to-list 'color-themes '(color-theme-ir-black "IR Black" "Burke Libbey"))
 (provide 'color-theme-ir-black)
 
 ;;; color-theme-ir-black.el ends here
