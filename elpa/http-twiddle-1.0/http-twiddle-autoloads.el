3d361206e2cd0ad905867f40da3fb3237cfb939e
jtimberman <joshua@opscode.com>
2012-02-08 21:58:34 -0700

    Remove emacs-starter-kit remnants and ELPA packages

--- elpa/http-twiddle-1.0/http-twiddle-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; http-twiddle-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("http-twiddle-pkg.el" "http-twiddle.el")
-;;;;;;  (20089 13631 427061))
-
-;;;***
-
-(provide 'http-twiddle-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; http-twiddle-autoloads.el ends here
--- elpa/http-twiddle-1.0/http-twiddle-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "http-twiddle" "1.0" "send & twiddle & resend HTTP requests" (quote nil))
