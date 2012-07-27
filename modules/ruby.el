5c9a2d7528233f5c8f2d8049b4bfb15f15328824
jtimberman <joshua@opscode.com>
2012-07-26 18:00:25 -0600

    enable flymake for ruby

--- modules/ruby.el
+++ modules/ruby.el
@@ -20,3 +20,33 @@
 (add-to-list 'auto-mode-alist '("Cheffile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Berksfile$" . ruby-mode))
 (add-hook 'ruby-mode-hook 'esk-paredit-nonlisp)
+
+
+(require 'flymake)
+
+;; Invoke ruby with '-c' to get syntax checking
+(defun flymake-ruby-init ()
+  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+         (local-file  (file-relative-name
+                       temp-file
+                       (file-name-directory buffer-file-name))))
+    (list "ruby" (list "-c" local-file))))
+
+(push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("Thorfile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("Cheffile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("Procfile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("Kitchenfile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("Cheffile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("Berksfile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("Vagrantfile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+
+(push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
+
+(add-hook 'ruby-mode-hook
+          '(lambda ()
+             (if (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
+                 (flymake-mode))
+             ))
