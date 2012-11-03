a24fe46706ce0b94f157f312c4fe1f97affa4024
jtimberman <joshua@opscode.com>
2012-11-03 10:23:10 -0600

    add rbenv pathing

--- modules/ruby.el
+++ modules/ruby.el
@@ -58,3 +58,7 @@
              (if (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
                  (flymake-mode))
              ))
+
+;; Setting rbenv path
+(setenv "PATH" (concat (getenv "HOME") "/.rbenv/shims:" (getenv "HOME") "/.rbenv/bin:" (getenv "PATH")))
+(setq exec-path (cons (concat (getenv "HOME") "/.rbenv/shims") (cons (concat (getenv "HOME") "/.rbenv/bin") exec-path)))
