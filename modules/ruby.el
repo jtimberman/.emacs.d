59c9b2c4a32231b0722bbaf4fe51f5808a82ce99
jtimberman <joshua@opscode.com>
2012-11-03 09:50:10 -0600

    add some new Rubyfiles

--- modules/ruby.el
+++ modules/ruby.el
@@ -12,13 +12,17 @@
 (setq ruby-deep-indent-paren nil)
 ;; knife, thor, chef and proc are ruby, others are in esk.
 (add-to-list 'auto-mode-alist '("\\.knife$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Thorfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Cheffile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Procfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Kitchenfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Cheffile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Berksfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Cheffile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Collanderfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Kitchenfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Procfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rantfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Thorfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
 (add-hook 'ruby-mode-hook 'esk-paredit-nonlisp)
 
 
