cfa87a560ca0db941c84e7a116f69c48926bbcd7
jtimberman <joshua@opscode.com>
2013-12-08 22:12:37 -0700

    dash at point settings

---
+++ modules/dash.el
@@ -0,0 +1,5 @@
+(add-to-list 'dash-at-point-mode-alist '(enh-ruby-mode . "ruby"))
+(add-to-list 'dash-at-point-mode-alist '(erlang-mode . "erlang"))
+(add-to-list 'dash-at-point-mode-alist '(go-mode . "go"))
+(add-to-list 'dash-at-point-mode-alist '(emacs-lisp-mode . "emacs-lisp"))
+
