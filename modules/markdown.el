d96be241db5cac9fcb95f55b3fe06e5c73839483
jtimberman <joshua@chef.io>
2014-12-21 19:55:50 -0700

    no autofill for markdown really

--- modules/markdown.el
+++ modules/markdown.el
@@ -3,3 +3,5 @@
 (setq auto-mode-alist (cons '("\\.mkd" . markdown-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.text" . markdown-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.txt" . markdown-mode) auto-mode-alist))
+;; auto-fill-mode, just say no
+(remove-hook 'text-mode-hook 'turn-on-auto-fill)
