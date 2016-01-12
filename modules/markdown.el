b23cf33097425d26d0844faf57ab2435fd1e919f
jtimberman <joshua@chef.io>
2016-01-12 15:39:30 -0700

    use GHFM for markdown

--- modules/markdown.el
+++ modules/markdown.el
@@ -1,7 +1,7 @@
-(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.mkd" . markdown-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.text" . markdown-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.txt" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.md" . gfm-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.markdown" . gfm-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.mkd" . gfm-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.text" . gfm-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.txt" . gfm-mode) auto-mode-alist))
 ;; auto-fill-mode, just say no
 (remove-hook 'text-mode-hook 'turn-on-auto-fill)
