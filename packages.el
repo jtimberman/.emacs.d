f5d0c13b70aa50e95a638422bda303172f5c1d58
jtimberman <joshua@chef.io>
2018-05-30 10:46:46 -0600

    update melpa to stable repo
    Signed-off-by: Joshua Timberman <joshua@chef.io>

--- packages.el
+++ packages.el
@@ -7,7 +7,8 @@
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")))
+
 
 (package-initialize)
 
